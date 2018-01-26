f365198b11f8fadb9d5f5de40ec5375ccca14583
Hyeseong Kim <cometkim.kr@gmail.com>
2018-01-27 00:49:27 +0900

    Separate the outgoing webhook typedefs

--- flow/model/index.js
+++ flow/model/index.js
@@ -30,6 +30,9 @@ declare module 'mattermost-typed/model' {
     declare export type IncomingWebhook = mattermost$IncomingWebhook
     declare export type IncomingWebhookPayload = mattermost$IncomingWebhookPayload
 
+    declare export type OutgoingWebhook = mattermost$OutgoingWebhook
+    declare export type OutgoingWebhookPayload = mattermost$OutgoingWebhookPayload
+
     declare export type ServerConfig = mattermost$ServerConfig;
     declare export type ClientConfig = mattermost$ClientConfig;
     declare export type LicenseInfo = mattermost$LicenseInfo;

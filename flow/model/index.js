c8c4f3f8047f547b9cb635ac2e4513a394fe6d34
Hyeseong Kim <cometkim.kr@gmail.com>
2018-01-27 00:21:19 +0900

    Separate the incoming webhook typedefs

--- flow/model/index.js
+++ flow/model/index.js
@@ -27,6 +27,9 @@ declare module 'mattermost-typed/model' {
 
     declare export type SlackAttachment = mattermost$SlackAttachment
 
+    declare export type IncomingWebhook = mattermost$IncomingWebhook
+    declare export type IncomingWebhookPayload = mattermost$IncomingWebhookPayload
+
     declare export type ServerConfig = mattermost$ServerConfig;
     declare export type ClientConfig = mattermost$ClientConfig;
     declare export type LicenseInfo = mattermost$LicenseInfo;

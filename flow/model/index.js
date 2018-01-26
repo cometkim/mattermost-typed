ded7110241644084f4ad0cb1b366cfb8997fe902
Hyeseong Kim <cometkim.kr@gmail.com>
2018-01-26 16:00:48 +0900

    Separate the team and team-relative typedefs

--- flow/model/index.js
+++ flow/model/index.js
@@ -6,6 +6,14 @@ declare module 'mattermost-typed/model' {
     declare export type UserSearch = mattermost$UserSearch
     declare export type UserAutoComplete = mattermost$UserAutocomplete
 
+    declare export type Team = mattermost$Team
+    declare export type TeamPatch = mattermost$TeamPatch
+    declare export type TeamStats = mattermost$TeamStats
+    declare export type TeamInviteInfo = mattermost$TeamInviteInfo
+    
+    declare export type TeamMember = mattermost$TeamMember
+    declare export type TeamUnread = mattermost$TeamUnread
+
     declare export type ServerConfig = mattermost$ServerConfig;
     declare export type ClientConfig = mattermost$ClientConfig;
     declare export type LicenseInfo = mattermost$LicenseInfo;
